99a35283e2cb94fac5229807d46fbe790a5d9d7a
bksuro <bksuro89@gmail.com>
2020-08-27 11:51:54 -0700

    Adding images and queries for project

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -106,9 +106,12 @@ ON ce.emp_no = de.emp_no
 GROUP BY de.dept_no
 ORDER BY de.dept_no;
 
+SELECT COUNT(emp_no)
+FROM current_emp;
+
 -- Create a new table of numbr of employees by dept retiring
 SELECT COUNT(ce.emp_no), de.dept_no
-INTO retiring_emp_count_by_dept
+-- INTO retiring_emp_count_by_dept
 FROM current_emp as ce
 LEFT JOIN dept_emp as de
 ON ce.emp_no = de.emp_no
@@ -138,6 +141,9 @@ WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
   AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31')
   AND (de.to_date = '9999-01-01');
   
+SELECT COUNT(emp_no)
+FROM emp_info;
+
 -- List of managers per department
 SELECT  dm.dept_no,
         d.dept_name,
@@ -164,6 +170,9 @@ INNER JOIN dept_emp AS de
   ON (ce.emp_no = de.emp_no)
 INNER JOIN departments AS d
   ON (de.dept_no = d.dept_no);
+  
+SELECT COUNT(emp_no)
+FROM dept_info;
 
 -- creating dept_info_sales table
 SELECT ce.emp_no,
@@ -191,3 +200,10 @@ INNER JOIN dept_emp AS de
 INNER JOIN departments AS d
   ON (de.dept_no = d.dept_no)
 WHERE (de.dept_no IN ('d007','d005'));
+
+SELECT * 
+FROM dept_info_sales_and_development;
+
+SELECT COUNT(dept_name), dept_name
+FROM dept_info_sales_and_development
+GROUP BY dept_name;
